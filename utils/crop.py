ea7f98dee3fd1c5f058f6643e5705013b8ad9311
fuzzythecat <fuzzythecat@gmail.com>
2016-05-30 01:27:53 +0900

    comment added

--- utils/crop.py
+++ utils/crop.py
@@ -1,13 +1,41 @@
 import random
 import numpy
 
-def crop(img):
-    #TODO: crop 40 11*11 segment from each slice(:,:,2:9)
+def crop(img, **kwargs):
+    """
+    From 64*64 images of 30 time frames and z-slice-no's [2:9] (default),
+    generate N = 2400 (default) random patches of size 11 (default).
+
+    Parameters
+    ----------
+    img : numpy array of dim 4 (shape: (64, 64, time, z)).
+
+    (optional)
+    len : Length of the sides of generated patches.
+    Defaults to 11.
+
+    patch_no : Number of the generated patches per slice.
+    Defaults to 10.
+
+    zidx : List of z-indices of input image to be cropped from.
+    Defaults to [2 : 9]
+
+    Returns
+    -------
+    patch : array of dim 3 (shape (idx, len, len)).
+    Idx is the total number of generated patches. (default : 10*30*8 = 2400)
+    Each patch is of size len*len (default : 11*11).
+    """
+
+    if img is None:
+        raise AttributeError("User must provide <numpy.ndarray> image")
 
     lim = 11
     tl = img.shape[2]
 
     idx = 0
+
+    # 9600 = 8 * 30 * 40
     patch = numpy.zeros((9600, lim, lim))
 
     for z in range(2, 10):
