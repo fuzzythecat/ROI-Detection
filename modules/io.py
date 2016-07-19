8801126ff21ab93d6b97b36d226bc15e6e092423
fuzzythecat <fuzzythecat@gmail.com>
2016-07-19 21:20:42 +0900

    added routine for loading .dcm files from directory

--- modules/io.py
+++ modules/io.py
@@ -1,4 +1,81 @@
+import os
+import sys
 from modules import conf
+
+def load_cine_from_directory(cinedir):
+    import dicom
+    import numpy as np
+
+    os.chdir(cinedir)
+    session_files = os.listdir()
+
+    zslice = 0
+    cine_series = []
+
+    for f in session_files:
+        if("DS" in f): continue
+        if len(f) <= 3:
+            cine_series.append(f)
+            zslice += 1
+
+    zcount = 0
+    print("loading cine data", end="")
+
+    for i in range(0, zslice):
+        # print("cine series: %s" % cine_series[i])
+        cwdir = cinedir + cine_series[i]
+        os.chdir(cwdir)
+        session_files = os.listdir()
+        # print(session_files)
+
+        if(i % (zslice/5) == 0):
+            print(".", end="", flush=True)
+
+        tcount = 0
+        for f in session_files:
+            if("DS" in f): continue
+            ds = dicom.read_file(f)
+            # print(ds.PatientName+ds.Rows+ds.Columns)
+            if zcount == 0 and tcount == 0:
+                pixelDims = (int(ds.Rows),
+                        int(ds.Columns), int(30), int(zslice))
+                cine_img = np.zeros(pixelDims, dtype=ds.pixel_array.dtype)
+
+            cine_img[:, :, tcount, zcount] = ds.pixel_array
+            tcount += 1
+        zcount += 1
+
+    print("\ndata dimension: ", end="")
+    print(cine_img.shape)
+
+    return cine_img
+
+
+def get_directory(**kwargs):
+    """
+    Wrapper function for PyQt4.QtGui.QFileDialog.getExistingDirectory().
+    Retreives name and directory of file to be opened.
+
+    Parameters
+    ----------
+    (optional)
+    initialdir : Initial directory displayed in file dialog.
+    Defaults to /data directory.
+
+    filetypes : Sequence of (label, pattern) tuples. Use '*' as the pattern
+    to indicate all files. Defaults to ('numpy files', '.npy').
+
+    Returns
+    -------
+    filename : 1-dimensional string of
+    absolute directory + name of the selected file.
+    """
+    from PyQt4 import QtGui
+
+    filename = str(QtGui.QFileDialog.getExistingDirectory())
+
+    return filename
+
 
 def get_file_path(**kwargs):
     """
